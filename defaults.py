4d688a26498675c2e903aa7e5a1fda0e18efddd1
Glen Smith <gsmith@praxismi.com>
2014-02-07 10:26:00 -0500

    Added comments to defaults

--- defaults.py
+++ defaults.py
@@ -3,10 +3,10 @@
 DONT_TOUCH = ('40.0.61.128', '74.93.92.201')
 # The security group we want to update
 PARENT_NAME = 'dev'
-OPEN_PORTS = (('tcp', '80', '80'),
-              ('tcp', '443', '443'),
-              ('tcp', '22', '22'),
-              ('tcp', '5672', '5672'),
-              ('tcp', '55672', '55672'))
+OPEN_PORTS = (('tcp', '80', '80'),  # HTTP
+              ('tcp', '443', '443'), # HTTPs
+              ('tcp', '22', '22'),  #SSH
+              ('tcp', '5672', '5672'),   #RabbitMQ
+              ('tcp', '55672', '55672'))  #RabbitMQ management plug-in
 
 
